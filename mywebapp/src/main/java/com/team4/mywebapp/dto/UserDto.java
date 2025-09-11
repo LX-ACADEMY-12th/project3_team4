37863c8cc50501e14dd511264376b8be911fb6ac
jiseoheo <jiseo.heo@gmail.com>
2025-09-11 20:55:42 +0900

    UserDto 수정

--- mywebapp/src/main/java/com/team4/mywebapp/dto/UserDto.java
+++ mywebapp/src/main/java/com/team4/mywebapp/dto/UserDto.java
@@ -5,14 +5,112 @@ import lombok.Data;
 
 
 public class UserDto {
-	private Long userId;
-	private String userName;
-	private String password;
+	private int userId;
+	private String loginId;
+	private String loginPW;
 	private String nickname;
-	private String gender;
-	private String profileImage;
+	private String profilePhotoPath;
 	private String statusMessage;
-	private LocalDateTime createdAt;
-	private LocalDateTime updatedAt;
+	private String birthDate;
+	private String region;
+	private String hobby;
+	private String gender;
+	
+	public int getUserId() {
+	    return userId;
+	}
+
+	public String getLoginId() {
+	    return loginId;
+	}
+
+	public String getLoginPW() {
+	    return loginPW;
+	}
+
+	public String getNickname() {
+	    return nickname;
+	}
+
+	public String getProfilePhotoPath() {
+	    return profilePhotoPath;
+	}
+
+	public String getStatusMessage() {
+	    return statusMessage;
+	}
+
+	public String getBirthDate() {
+	    return birthDate;
+	}
+
+	public String getRegion() {
+	    return region;
+	}
+
+	public String getHobby() {
+	    return hobby;
+	}
+
+	public String getGender() {
+	    return gender;
+	}
+
+	// Setter methods
+	public void setUserId(int userId) {
+	    this.userId = userId;
+	}
+
+	public void setLoginId(String loginId) {
+	    this.loginId = loginId;
+	}
+
+	public void setLoginPW(String loginPW) {
+	    this.loginPW = loginPW;
+	}
+
+	public void setNickname(String nickname) {
+	    this.nickname = nickname;
+	}
+
+	public void setProfilePhotoPath(String profilePhotoPath) {
+	    this.profilePhotoPath = profilePhotoPath;
+	}
+
+	public void setStatusMessage(String statusMessage) {
+	    this.statusMessage = statusMessage;
+	}
+
+	public void setBirthDate(String birthDate) {
+	    this.birthDate = birthDate;
+	}
+
+	public void setRegion(String region) {
+	    this.region = region;
+	}
+
+	public void setHobby(String hobby) {
+	    this.hobby = hobby;
+	}
+
+	public void setGender(String gender) {
+	    this.gender = gender;
+	}
+	
+	@Override
+	public String toString() {
+	    return "User{" +
+	            "userId=" + userId +
+	            ", loginId='" + loginId + '\'' +
+	            ", loginPW='" + loginPW + '\'' +
+	            ", nickname='" + nickname + '\'' +
+	            ", profilePhotoPath='" + profilePhotoPath + '\'' +
+	            ", statusMessage='" + statusMessage + '\'' +
+	            ", birthDate='" + birthDate + '\'' +
+	            ", region='" + region + '\'' +
+	            ", hobby='" + hobby + '\'' +
+	            ", gender='" + gender + '\'' +
+	            '}';
+	}
 	
 }
