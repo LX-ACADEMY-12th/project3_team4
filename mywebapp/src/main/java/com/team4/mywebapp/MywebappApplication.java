0c718a7952a221e5a2fbb35f7a0d802d249c660c
yunjungeee <bagyunjeong49@gmail.com>
2025-09-12 09:49:55 +0900

    Application에 어노테이션 추가, loginController

--- mywebapp/src/main/java/com/team4/mywebapp/MywebappApplication.java
+++ mywebapp/src/main/java/com/team4/mywebapp/MywebappApplication.java
@@ -1,9 +1,12 @@
 package com.team4.mywebapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan("UserMapper")
 public class MywebappApplication {
 
 	public static void main(String[] args) {
--- mywebapp/src/main/java/com/team4/mywebapp/controller/AuthController.java
+++ mywebapp/src/main/java/com/team4/mywebapp/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.team4.mywebapp.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,32 +10,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.team4.mywebapp.MywebappApplication;
 import com.team4.mywebapp.dto.UserDto;
 import com.team4.mywebapp.service.AuthService;
 
+import jakarta.servlet.http.HttpSession;
 
 
 @RestController
 @RequestMapping("/api")  // ← API 경로
 @CrossOrigin(origins = "http://localhost:3000")  // ← Vue.js 개발서버 CORS
 public class AuthController {
+
+    private final MywebappApplication mywebappApplication;
 	
 	@Autowired
 	private AuthService authService;
+
+    AuthController(MywebappApplication mywebappApplication) {
+        this.mywebappApplication = mywebappApplication;
+    }
 	
 	// 로그인 요청 처리
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody UserDto UserDto) {
-        // 비즈니스 로직은 서비스 계층에 위임
+	public ResponseEntity<String> login(@RequestBody UserDto UserDto, HttpSession session) {
+		Map<String, Object> response = new HashMap<>();
+		
+		System.out.println("입력받은 아이디: " + UserDto.getLoginId());
+		System.out.println("입력받은 비밀번호: " + UserDto.getLoginPw());
+		
+        // 비즈니스 로직은 서비스 계층에 위임 AuthService를 사용해서 로그인 인증
         boolean isAuthenticated = authService.authenticateUser(UserDto);
 
         if (isAuthenticated) {
             // 인증 성공 시, 성공 응답 반환
             // (실제로는 JWT 토큰 등을 생성하여 반환)
+        	session.setAttribute("loginId", UserDto.getLoginId());
+        	response.put("success", true);
             return ResponseEntity.ok("로그인 성공");
         } else {
             // 인증 실패 시, 401 Unauthorized 응답 반환
+        	response.put("success", false);
             return ResponseEntity.badRequest().body("아이디 또는 비밀번호가 잘못되었습니다.");
         }
     }
