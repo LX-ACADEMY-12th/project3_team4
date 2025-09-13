8653d220d00aba238762711cde2991f66ec172fa
jiseoheo <jiseo.heo@gmail.com>
2025-09-13 16:31:57 +0900

    0913--입력갑 String을 바꿈

--- mywebapp/src/main/java/com/team4/mywebapp/controller/SignupController.java
+++ mywebapp/src/main/java/com/team4/mywebapp/controller/SignupController.java
@@ -1,13 +1,10 @@
 package com.team4.mywebapp.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.team4.mywebapp.dto.SignupDto;
 import com.team4.mywebapp.service.SignupService;
 
@@ -19,18 +16,34 @@ public class SignupController {
 	@Autowired
 	private SignupService signupService;
 	
+	// 아이디 중복확인
+	@PostMapping("/checkDuplicate")
+	public String checkDuplicate(@RequestBody SignupDto inputUser) throws Exception {
+	    try {
+	        boolean isLoginIdExists = signupService.checkLoginIdExists(inputUser);
+	        if (isLoginIdExists) {
+	            System.out.println("아이디 중복: " + inputUser.getLoginId());
+	            return "duplicate";
+	        } else {
+	            System.out.println("아이디 사용 가능: " + inputUser.getLoginId());
+	            return "available";
+	        }
+	    } catch(Exception e) {
+	        System.err.println("controller: 중복확인 오류: " + e.getMessage());
+	        return "error";
+	    }
+	}
+	
 	// 회원가입
 	@PostMapping("/insert")
 	public String insert(@RequestBody SignupDto inputUser) throws Exception{
 	    try {
 	        boolean isLoginIdExists = signupService.checkLoginIdExists(inputUser);
-
 	        if (isLoginIdExists) {
 	            System.err.println("loginId에서 중복 감지됨");
 	            return "duplicate"; // redirect 대신 단순 문자열 반환
 	        } else {
 	            boolean isInserted = signupService.insertUser(inputUser);
-
 	            if (isInserted) {
 	                System.out.println("회원가입 성공");
 	                return "success"; // redirect 대신 단순 문자열 반환
@@ -44,5 +57,4 @@ public class SignupController {
 	        return "error"; // redirect 대신 단순 문자열 반환
 	    }
 	}
-
 }
