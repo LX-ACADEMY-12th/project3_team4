6a3dc32026495ab9beeaf8edc8fb7b25ff362360
jiseoheo <jiseo.heo@gmail.com>
2025-09-13 14:02:54 +0900

    signup backend 경로 이상 수정

--- mywebapp/src/main/java/com/team4/mywebapp/controller/SecurityConfig.java
+++ mywebapp/src/main/java/com/team4/mywebapp/controller/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.team4.mywebapp.config;
+package com.team4.mywebapp.controller;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
