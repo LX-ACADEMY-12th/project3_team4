86c49991c0289ef5178c3ce5e30a1436de36209c
yunjungeee <bagyunjeong49@gmail.com>
2025-09-13 20:02:40 +0900

    Theme 기능구현완

--- mywebapp/src/main/java/com/team4/mywebapp/controller/ThemeController.java
+++ mywebapp/src/main/java/com/team4/mywebapp/controller/ThemeController.java
@@ -1,5 +1,37 @@
 package com.team4.mywebapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.team4.mywebapp.dto.ThemeDto;
+import com.team4.mywebapp.service.ThemeService;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ThemeController {
+	
+	@Autowired
+	private ThemeService themeService;
+	
+	@GetMapping("/theme-list")
+	public ResponseEntity<List<ThemeDto>> list() {		
+		List<ThemeDto> list = themeService.getAllThemes();
+		return ResponseEntity.ok(list);		
+	}
+	
+	@PostMapping("/theme-update")
+	public ResponseEntity<Integer> update(@RequestBody ThemeDto themeDto) {
+		int result = themeService.updateTheme(themeDto);
+		return ResponseEntity.ok(result);
+		
+	}
 
 }
--- mywebapp/src/main/java/com/team4/mywebapp/service/ThemeService.java
+++ mywebapp/src/main/java/com/team4/mywebapp/service/ThemeService.java
@@ -1,5 +1,28 @@
 package com.team4.mywebapp.service;
 
-public interface ThemeService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.team4.mywebapp.dto.ThemeDto;
+import com.team4.mywebapp.mapper.ThemeMapper;
+
+@Service
+public class ThemeService {
+	
+	@Autowired
+	private ThemeMapper themeMapper;
+	
+	// 테마 조회
+	public List<ThemeDto> getAllThemes() {
+		return themeMapper.getAllThemes();		
+	}
+	
+	// 테마 수정
+	public int updateTheme(ThemeDto themeDto) {
+		return themeMapper.updateTheme(themeDto);		
+	}
+	
 
 }
