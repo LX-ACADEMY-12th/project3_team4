36b0671424536219f56e66cb7c875d10d6f9c48f
jiseoheo <jiseo.heo@gmail.com>
2025-09-14 20:21:24 +0900

    0914--yj3와 동기화

---
+++ mywebapp/src/main/java/com/team4/mywebapp/dto/UserModifyDto.java
@@ -0,0 +1,79 @@
+package com.team4.mywebapp.dto;
+
+import java.util.Date;
+
+public class UserModifyDto {
+	
+	
+	private String profilePhotoPath;
+	private Date birthDate;
+	private String gender;
+	private String region;
+	private String hobby;
+	private String statusMessage;
+	private String todayMood;
+	private int minihomeId;
+	private int userId;
+	
+	public String getProfilePhotoPath() {
+		return profilePhotoPath;
+	}
+	public void setProfilePhotoPath(String profilePhotoPath) {
+		this.profilePhotoPath = profilePhotoPath;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getHobby() {
+		return hobby;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+	public String getTodayMood() {
+		return todayMood;
+	}
+	public void setTodayMood(String todayMood) {
+		this.todayMood = todayMood;
+	}
+	public int getMinihomeId() {
+		return minihomeId;
+	}
+	public void setMinihomeId(int minihomeId) {
+		this.minihomeId = minihomeId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
+	
+	
+	
+	
+
+}
