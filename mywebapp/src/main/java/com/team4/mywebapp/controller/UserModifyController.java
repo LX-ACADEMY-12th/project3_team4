faaa6846c5d206d62e06e815723cb093d60c2e62
yunjungeee <bagyunjeong49@gmail.com>
2025-09-15 09:50:51 +0900

    25년 09월 15일

--- mywebapp/src/main/java/com/team4/mywebapp/controller/UserModifyController.java
+++ mywebapp/src/main/java/com/team4/mywebapp/controller/UserModifyController.java
@@ -2,6 +2,7 @@ package com.team4.mywebapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserModifyController {
 	
 	@Autowired
